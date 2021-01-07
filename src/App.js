e48f09e725437220647091cbc4550f5aec74f7de
TomozQ <inf.4clover@gmail.com>
2021-01-07 22:16:06 +0900

    createComponent

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import React from 'react'
+import { useDropzone } from 'react-dropzone'
 import './App.css';
 
 function App() {
+  const {acceptedFiles, getRootProps, getInputProps} = useDropzone()
   return (
     <>
-      This is First Commit
+      <div { ...getRootProps({ className: 'dropzone' })}>
+        <ul className='imageSpace'>
+          <input {...getInputProps()} className='hidden' />
+          <p>Drag and Drop</p>
+          <div className='imageSpot'>
+            {acceptedFiles.map((acceptedFile, index) => {
+              const createObjectURL = ( window.URL || window.webkitURL ).createObjectURL
+              return(
+                <li key={ index } className='imageList'>
+                  <img src={ createObjectURL( acceptedFile )} alt='preview'/>
+                </li>
+              )
+            })}
+          </div>
+        </ul>
+
+      </div>
     </>
   );
 }
